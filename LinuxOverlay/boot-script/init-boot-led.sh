c4d40ce8cdf2cc9adb1b07fd7a3dc0d2c63991fe
bernardlee99 <bernardlee999@gmail.com>
2020-08-18 23:06:48 -0500

    Linux PWM LED status indicator implementation upon successful boot

---
+++ LinuxOverlay/boot-script/init-boot-led.sh
@@ -0,0 +1,32 @@
+INIT_LED_VALUE=0
+INIT_BOOT_COMPLETE=0
+INIT_BOOT_UP=1
+
+echo 1000000 > /sys/class/pwm/pwmchip2/pwm0/period
+echo $INIT_LED_VALUE > /sys/class/pwm/pwmchip2/pwm0/duty_cycle
+echo 1 > /sys/class/pwm/pwmchip2/pwm0/enable
+
+while [ $INIT_BOOT_COMPLETE == 0 ]
+do
+    
+    if [[ $INIT_BOOT_UP == 1 ]]
+    then
+        INIT_LED_VALUE=$(($INIT_LED_VALUE+5000))
+    else 
+        INIT_LED_VALUE=$(($INIT_LED_VALUE-5000))
+    fi
+    
+
+    if [[ $INIT_LED_VALUE -gt 999999 && $INIT_BOOT_UP == 1 ]]
+    then
+        INIT_BOOT_UP=0
+    elif [[ $INIT_LED_VALUE -lt 1 && $INIT_BOOT_UP == 0 ]]
+    then
+        INIT_BOOT_UP=1
+    fi
+
+    echo $INIT_LED_VALUE > /sys/class/pwm/pwmchip2/pwm0/duty_cycle
+
+    sleep 0.05
+    
+done
--- LinuxOverlay/test.sh
+++ LinuxOverlay/test.sh
@@ -1,28 +1,8 @@
-LDR_ADC_READ=0
-LDR_ADC_READ_AVG=0
-LDR_ADC_READ_FINAL=0
-LDR_ADC_READ_COUNT=999
-
-echo 0 > /sys/class/pwm/pwmchip2/export
-echo 1000000 > /sys/class/pwm/pwmchip2/pwm0/period
-echo 1 > /sys/class/pwm/pwmchip2/pwm0/enable
-
-while true
+#!/bin/sh
+echo 45 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio45/direction
+while :
 do
-    LDR_ADC_READ=$(cat /sys/bus/iio/devices/iio\:device0/in_voltage0_raw)
-    LDR_ADC_READ_AVG=$(($LDR_ADC_READ+$LDR_ADC_READ_AVG))
-    LDR_ADC_READ_COUNT=$(($LDR_ADC_READ_COUNT+1))
-
-    if [[ $LDR_ADC_READ_COUNT -gt 1000 ]]
-    then
-        PWM_WRITE=$(($LDR_ADC_READ_AVG*243/1000))
-        if [[ $PWM_WRITE -gt 1000000 ]]
-        then
-            $PWM_WRITE = 1000000
-        fi
-        echo $PWM_WRITE > /sys/class/pwm/pwmchip2/pwm0/duty_cycle
-        LDR_ADC_READ_COUNT=0
-        LDR_ADC_READ_AVG=0
-    fi
-    
+    echo 1 > /sys/class/gpio/gpio45/value 
+    echo 0 > /sys/class/gpio/gpio45/value 
 done
